e5fb682e6062cf46f0f3c89349d4cd1e1b482ba3
shreya2123 <shreyashreesgowda21@gmail.com>
2026-03-27 15:28:42 +0530

    slpash screen seen

---
+++ splash_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => LoginScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [Colors.purple, Colors.pink]),
+        ),
+        child: Center(
+          child: Text(
+            "💊 MedReminder",
+            style: TextStyle(
+                fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
